1532c0e364e6e8eccb34ffd7fd4aac3a33f34386
ahin0119 <ahin0119@naver.com>
2023-12-02 04:26:08 +0900

    Update Shooter Script

--- EGG_ALONGUP/Assets/Scripts/Map/Shooter.cs
+++ EGG_ALONGUP/Assets/Scripts/Map/Shooter.cs
@@ -7,6 +7,7 @@ public class Shooter : MonoBehaviour
     public GameObject obstaclePrefab; // 생성할 작은 오브젝트 프리팹
     public float spawnInterval = 2.0f; // 작은 오브젝트를 생성하는 간격
     public float obstacleSpeed = 5.0f; // 작은 오브젝트의 이동 속도
+    public float obstacleLifetime = 3.0f; // 발사체가 사라지는 시간
 
     private float spawnTimer = 0.0f;
 
@@ -26,8 +27,11 @@ public class Shooter : MonoBehaviour
         Rigidbody obstacleRigidbody = obstacle.GetComponent<Rigidbody>();
         if (obstacleRigidbody != null)
         {
-            obstacleRigidbody.velocity = transform.up * obstacleSpeed; //투사체 방향
+            obstacleRigidbody.velocity = transform.up * obstacleSpeed; // 발사체 방향
             obstacleRigidbody.useGravity = false; // 중력 영향 제거
+
+            // 일정 시간이 지난 후에 발사체 제거
+            Destroy(obstacle, obstacleLifetime);
         }
     }
 }
